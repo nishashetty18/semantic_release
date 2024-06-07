c666232f57f76e279994a6045b658c515ff2bcb7
Nisha <shettynishi55@gmail.com>
2024-06-07 16:26:31 +0530

    fix: bug fixes

--- release.config.js
+++ release.config.js
@@ -9,8 +9,8 @@ module.exports = {
       [
         '@semantic-release/git',
         {
-          assets: ['CHANGELOG.md', 'package.json'],
-          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            "assets": ["package.json", "CHANGELOG.md"],
+            "message": "chore(release): ${nextRelease.version} [skip ci]"
         }
       ]
     ]
